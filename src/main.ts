20f2e33137d3895e757ade4bcf329beb07dcbe80
oscarhandsome <altair5555@gmail.com>
2024-02-02 13:12:38 +0600

    fixed loading standalone comps

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import './assets/css/main.css'
 
-import { createApp } from 'vue'
+import { createApp, defineAsyncComponent } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
-import TabOverview from '@/components/Tabs/Tab-overview.vue'
-import TabTransactionsHistory from '@/components/Tabs/Tab-transactions-history.vue'
+// import TabOverview from '@/components/Tabs/Tab-overview.vue'
+// import TabTransactionsHistory from '@/components/Tabs/Tab-transactions-history.vue'
 
 const app = createApp(App)
 
@@ -15,7 +15,13 @@ app.use(createPinia())
 app.use(router)
 
 // app.component('TheHeader', TheHeader)
-app.component('TabOverview', TabOverview)
-app.component('TabTransactionsHistory', TabTransactionsHistory)
+app.component(
+  'TabOverview',
+  defineAsyncComponent(() => import('@/components/Tabs/Tab-overview.vue'))
+)
+app.component(
+  'TabTransactionsHistory',
+  defineAsyncComponent(() => import('@/components/Tabs/Tab-transactions-history.vue'))
+)
 
 app.mount('#app')
