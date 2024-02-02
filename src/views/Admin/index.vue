3ff7ac8ad5236d5404f927ea51d2d3c9816c7f9a
oscarhandsome <altair5555@gmail.com>
2024-02-03 04:31:56 +0600

    added childrens

---
+++ src/views/Admin/index.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div>
+    <h1>Admin</h1>
+
+    <router-link to="/admin" class="underline mr-2">Back</router-link>
+    <router-link to="/admin/settings" class="underline">Open admin settings</router-link>
+
+    <div class="border drop-shadow-sm p-2">
+      Child rourte:
+
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
---
+++ src/views/Admin/settings.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="border drop-shadow-sm p-2">
+    <h2>Admin settings:</h2>
+
+    <ul class="border drop-shadow-sm p-2">
+      <li>Color: sdasdasd</li>
+      <li>Font size: 12px</li>
+    </ul>
+  </div>
+</template>
