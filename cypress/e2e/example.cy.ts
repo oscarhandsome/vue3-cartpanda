2e1e80e645a5535571c134cae3e4d64bb28decf6
Askhat Bagaviyev <altair5555@gmail.com>
2024-02-17 20:55:58 +0600

    added example tests

--- cypress/e2e/example.cy.ts
+++ cypress/e2e/example.cy.ts
@@ -1,8 +1,14 @@
 // https://on.cypress.io/api
 
-describe('My First Test', () => {
+describe('Dashboard Page', () => {
   it('visits the app root url', () => {
     cy.visit('/')
-    cy.contains('h1', 'You did it!')
+    cy.contains('div', 'Dashboard')
+
+    cy.visit('/orders')
+    cy.contains('div', 'Orders')
+
+    cy.visit('/payments')
+    cy.contains('div', 'Payments')
   })
 })
